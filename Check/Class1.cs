f18beb93dd12b6a4d17f3f7800e18a68657ba2ab
nandi <nandi@DESKTOP-649JC5B>
2022-08-26 11:26:24 +0530

    Add project files.

---
+++ Check/Class1.cs
@@ -0,0 +1,57 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Check
+{
+    [TestFixture]
+    public abstract class Class1
+    {
+        protected ExtentReports _extent;
+        protected ExtentTest _test;
+        IWebDriver driver = new ChromeDriver();
+        [OneTimeSetUp]
+        protected void Setup()
+        {
+            var dir = TestContext.CurrentContext.TestDirectory + "\\";
+            var fileName = this.GetType().ToString() + ".html";
+            var htmlReporter = new ExtentHtmlReporter(@"C:\Users\nandi\source\repos\Check\Check\check.html");
+
+
+            _extent = new ExtentReports();
+            _extent.AttachReporter(htmlReporter);
+        }
+        [OneTimeTearDown]
+        protected void TearDown()
+        {
+            _extent.Flush();
+        }
+        [TestFixture]
+        public class TestInitializeWithNullValues : Class1
+        {
+            [Test]
+            public void LaunchBrowser()
+            {
+                driver.Navigate().GoToUrl("https://in.pg.com/");
+                driver.Manage().Window.Maximize();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                driver.FindElement(By.XPath("//span[.='Our Impact']")).Click();
+                driver.FindElement(By.XPath("//a[.='Sustainability']")).Click();
+                IWebElement p = driver.FindElement(By.XPath("//span[.='P&G sustainability goals for 2030']"));
+                string Text = p.Text;
+                Console.WriteLine("..........." + Text + ".");
+                driver.Quit();
+
+            }
+
+        }
+        
+    }
+}
---
+++ Check/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Check")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Check")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("66518e2d-4b06-43e0-a20e-546bfbf42765")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
